1c54661846306145d1fd75dbca47adff140e8925
Mike Lynch <mj.lynch23@gmail.com>
2023-07-17 16:22:03 -0700

    Adding update to Challenge #6

---
+++ Easy/6/perfect_numbers.rb
@@ -0,0 +1,24 @@
+require 'pry'
+
+class PerfectNumber
+  def self.classify(num)
+    raise(StandardError) if num < 1
+
+    tester = (1...num).each_with_object([]) { |digit, arr| arr << digit if num % digit == 0 }.sum
+    perfect_number(tester, num)
+  end
+
+  class << self
+    private
+
+    def perfect_number(sum, num)
+      if sum < num
+        'deficient'
+      elsif sum > num
+        'abundant'
+      else
+        'perfect'
+      end
+    end
+  end
+end
